58226332f2304c6a92ddb4d5fe93770d92879656
xgz <xgz@sise.com>
2024-01-09 09:21:45 +0800

    '第一次上传'

---
+++ miniprogram/pages/component/search/search.ts
@@ -0,0 +1,23 @@
+// pages/component/search/search.ts
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    placeholder:String,
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
---
+++ miniprogram/pages/index/index.ts
@@ -0,0 +1,37 @@
+// index.ts
+// 获取应用实例
+
+const app = getApp()
+
+Page({
+  data: {
+    navBarHeight: app.globalData.navBarHeight,
+        menuRight: app.globalData.menuRight,
+        menuTop: app.globalData.menuTop,
+        menuHeight: app.globalData.menuHeight,
+        placeholder:'请输入要查找的内容',
+        swiperimages:['../../static/index/indexbanner.png','../../static/index/indexbanner1.png','../../static/index/indexbanner2.png'],
+        tabList: [{
+					id: "tab01",
+					name: '三农政策'
+				}, {
+					id: "tab02",
+					name: '农业科技'
+				}],
+  },
+  onLoad() {
+      wx.request({
+        url: 'http://127.0.0.1:3002/getnews',
+        method: 'POST',
+        data: {type:1},
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        success(res) {
+          console.log(res)
+        }
+      })
+  
+    
+  }
+})
