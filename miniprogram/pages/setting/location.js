72d4b425e87c293e70bd7b6c48f5f276396ec73d
“xgz” <23>
2024-02-27 14:50:02 +0800

    feat: 管理地址功能静态搭建

--- miniprogram/pages/setting/location.js
+++ miniprogram/pages/setting/location.js
@@ -1,13 +1,27 @@
 // addressList/addressList.js
 
 Page({
+    data: {
+    Defaultadress: 1,
+    ismanager:false,
+  },
     goToEditAddress() {
       wx.navigateTo({
         url: './locationEdit',
       });
     },
+    changeDefaultadress(event){
+        this.setData({
+            Defaultadress: event.detail
+          });
+    },
     onClickLeft() {
         wx.navigateBack({ delta: 1 });
+      },
+      tapismanager(){
+          this.setData({
+            ismanager:!this.data.ismanager
+          })
       },
   });
   
